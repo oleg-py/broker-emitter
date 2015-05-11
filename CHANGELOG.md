b4f2c2793154e4570bdd367dc4974229fc20254d
Chen Liang <code@chen.technology>
2015-05-11 15:33:45 -0700

    `#emit(event, data, options)` supports passing `options` to `broker.router.publish`

---
+++ CHANGELOG.md
@@ -0,0 +1,8 @@
+# Change Log
+
+## v0.0.2 (2015-05-11)
+* chai@2.3.0
+* lodash@3.8.0
+* chai-as-promised@5.0.0
+* `broker-node@v0.0.9`
+* `#emit(event, data, options)` supports passing `options` to `broker.router.publish`
--- test/test.emitter.js
+++ test/test.emitter.js
@@ -10,18 +10,19 @@
  */
 var Promise = require('bluebird');
 var Emitter = require('./../lib/emitter');
+var debug = require('debug')('broker-emitter:test:emitter');
 
 describe('Emitter', function () {
-  describe('#on(event, listener)', function () {
-    this.timeout(20 * 1000);
-    before(function () {
-      var emitter = new Emitter({
-        uri: this.uri,
-        exchangeName: 'broker-emitter-test'
-      });
-      this.emitter = emitter;
-      return emitter.connect();
+  this.timeout(20 * 1000);
+  before(function () {
+    var emitter = new Emitter({
+      uri: this.uri,
+      exchangeName: 'broker-emitter-test'
     });
+    this.emitter = emitter;
+    return emitter.connect();
+  });
+  describe('#on(event, listener)', function () {
     it('receive message', function () {
       var self = this;
       var receivedMessage = [];
@@ -86,6 +87,45 @@ describe('Emitter', function () {
         })
         .then(function () {
           return expectation2;
+        });
+    });
+  });
+  describe('#emit(event, data, options)', function () {
+    it('should support options.headers', function () {
+      var self = this;
+      var receivedMessage = [];
+      var event = 'broker-test.emitheaders';
+      var resolver;
+      var expectation = new Promise(function (resolve, reject) {
+        resolver = resolve;
+      });
+      this.emitter.on(event, function (message) {
+        receivedMessage.push(message);
+        if (receivedMessage.length === 2) {
+          resolver();
+        }
+      });
+
+      return Promise.resolve().delay(1000)
+        .then(function () {
+          return self.emitter.emit(event, 'message-1', {
+            headers: {
+              taskName: 'testing.emitter.headers'
+            }
+          });
+        })
+        .then(function () {
+          return self.emitter.emit(event, 'message-2');
+        })
+        .then(function () {
+          return expectation;
+        })
+        .then(function () {
+          var firstMessage = receivedMessage[0];
+          // debug('firstMessage', firstMessage);
+          firstMessage.should.have.property('headers')
+            .that.has.property('taskName', 'testing.emitter.headers');
+          return Promise.resolve();
         });
     });
   });
